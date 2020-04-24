02abc3f8b002c1bdc63c25163d0cff352c224f24
Miguel Lopes <39331790+zmgalante@users.noreply.github.com>
2020-04-25 00:56:31 +0100

    Add cypress (#20)

--- src/pages/HowToPlay/HowToPlay.js
+++ src/pages/HowToPlay/HowToPlay.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
-const HowToPlay = () => <Link to="/">Go Back</Link>;
+const HowToPlay = () => (
+  <Link data-test="go-back-home-link" to={`${process.env.PUBLIC_URL}/`}>
+    Go Back
+  </Link>
+);
 
 export default HowToPlay;
