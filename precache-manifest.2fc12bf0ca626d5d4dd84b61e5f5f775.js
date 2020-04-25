3798b0367fd102ad19f1db826ac55366f7a171ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-25 20:30:40 +0000

    Deploy zmgalante/lletter6 to github.com/zmgalante/lletter6.git:gh-pages

---
+++ precache-manifest.2fc12bf0ca626d5d4dd84b61e5f5f775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f44fe90f7579cdccafb0c9e94a23e5f",
+    "url": "/lletter6/index.html"
+  },
+  {
+    "revision": "7e7c5fb46239e5bc0492",
+    "url": "/lletter6/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ae4385d8f4ff7fe0b1c",
+    "url": "/lletter6/static/js/2.7d1f6046.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lletter6/static/js/2.7d1f6046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7c5fb46239e5bc0492",
+    "url": "/lletter6/static/js/main.e4bf4043.chunk.js"
+  },
+  {
+    "revision": "994b153e1216dfee3734",
+    "url": "/lletter6/static/js/runtime-main.50b47052.js"
+  }
+]);
